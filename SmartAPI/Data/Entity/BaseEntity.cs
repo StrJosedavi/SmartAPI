2b8ac553463bc611eab2a5f78b71a9462344b086
StrJosedavi <josedavi100@gmail.com>
2023-07-15 00:33:47 -0300

    Feat: Ajustes nas entidades e adição de nova dependencia

--- SmartAPI/Data/Entity/BaseEntity.cs
+++ SmartAPI/Data/Entity/BaseEntity.cs
@@ -2,7 +2,7 @@
 {
     public class BaseEntity
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime UpdateDate { get; set; }
     }
