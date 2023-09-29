df3a97f47ee3b42691c15236f127139eb8cf6aaf
Fit-Josedavi <davi.sobreira@fitbank.com.br>
2023-09-29 12:02:35 -0300

    Feat: Implementando o bcrypt para criptografia de senha

--- SmartAPI/Util/Encrypt.cs
+++ SmartAPI/Util/Encrypt.cs
@@ -5,13 +5,11 @@ namespace SmartAPI.Util {
     public static class Encrypt {
         public static string GenerateHash(string Pwd) 
         {
-            var encodPass = Encoding.UTF8.GetBytes(Pwd);
-            int salt = new Random().Next(9000, 99999999);
-            byte[] saltBytes = Encoding.ASCII.GetBytes(salt.ToString());
-            var hmacSHA512 = new HMACSHA512(saltBytes);
-            byte[] saltedHash = hmacSHA512.ComputeHash(encodPass);
+            return BCrypt.Net.BCrypt.HashPassword(Pwd);
+        }
 
-            return Convert.ToBase64String(saltedHash);
+        public static bool ComparePassword(string password, string hash) {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
         }
     }
 }
