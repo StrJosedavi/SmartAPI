302f250fbb5baf7e0944c31ec7c96fb3dc41911d
Fit-Josedavi <davi.sobreira@fitbank.com.br>
2023-09-29 12:04:04 -0300

    Feat: Camada de Validação

---
+++ SmartAPI/Services/Validations/UserValidation.cs
@@ -0,0 +1,23 @@
+﻿using SmartAPI.Models.Request;
+
+namespace SmartAPI.Services.Validations {
+    public class UserValidation {
+        public static void ValidateRequest(UserRegisterRequest userRegisterRequest) 
+        {
+
+            if (string.IsNullOrEmpty(userRegisterRequest.Login) || userRegisterRequest.Login.Length > 50)
+                throw new Exception();
+
+            if (userRegisterRequest.Password != userRegisterRequest.ConfirmPassword)
+                throw new Exception();
+
+            if (userRegisterRequest.Password.Length > 64 || userRegisterRequest.Password.Length < 8)
+                throw new Exception();
+
+            if (string.IsNullOrEmpty(userRegisterRequest.Email) || !userRegisterRequest.Email.Contains("@"))
+                throw new Exception();
+        }
+
+
+    }
+}
